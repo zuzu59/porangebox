c8be64cf5424d1122155be40cb38ded57b93b0ea
Christian Zufferey <christian@zufferey.com>
2017-07-02 10:16:13 +0100

    - ajout du script install.sh (tout début ne marche pas encore !)

---
+++ install.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+# Petit script tout simple pour installer le soft PirateBox sur son OrangePi Zero avec DietPi comme OS
+# zf170702.1051
+# source: https://piratebox.cc/raspberry_pi:diy:manual
+
+
