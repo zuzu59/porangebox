3cfa1d9ba91e1cd13aa3588ebc361b371a757d52
Christian Zufferey <christian@zufferey.com>
2017-07-03 20:52:14 +0100

    - modifié le script install.sh afin qu'il patch le fichier hostapd.conf

--- install.sh
+++ install.sh
@@ -33,8 +33,12 @@ cd /opt/piratebox
 sed 's:DROOPY_USE_USER="no":DROOPY_USE_USER="yes":' -i /opt/piratebox/conf/piratebox.conf
 ln -s /opt/piratebox/init.d/piratebox /etc/init.d/piratebox
 update-rc.d piratebox defaults 
-/etc/init.d/piratebox start
 
-echo -------- patch le démarrage de piratbox pour arrêter en premier les services lighttpd, dnsmasq et hostap qui ont été redémarrés par DietPi lors du boot
+echo -------- patch le démarrage de piratebox pour arrêter en premier les services lighttpd, dnsmasq et hostap qui ont été redémarrés par DietPi lors du boot
 ./patch_piratebox_conf.sh
 
+echo -------- patch le fichier de conf de hostap afin d'enclencher le WIFI 802.11n
+./patch_hostapd.conf.sh
+
+echo -------- démarre le service piratebox
+/etc/init.d/piratebox start
--- patch_piratebox_conf.sh
+++ patch_piratebox_conf.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-# Petit script tout simple pour patcher le fichier de conf de piratebox pour qu'au démarrage de piratbox il arrête
+# Petit script tout simple pour patcher le fichier de conf de piratebox pour qu'au démarrage de piratebox il arrête
 # en premier les services lighttpd, dnsmasq et hostap qui ont été redémarrés par DietPi lors du boot
 # zf170703.2135
 # source: https://piratebox.cc/raspberry_pi:diy:manual
