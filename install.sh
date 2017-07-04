3ba579d59e7d04eb7179211cfc861f9dcf938c3b
Christian Zufferey <christian@zufferey.com>
2017-07-04 15:30:15 +0100

    - modifié le path de la conf wifi pour changer son ID et surtout le canal, pris le 8 au lieu du 1 !

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # Petit script tout simple pour installer le soft PirateBox sur son OrangePi Zero avec DietPi comme OS
-# zf170703.2133
+# zf170704.1634
 # source: https://piratebox.cc/raspberry_pi:diy:manual
 
 echo -------- installation des packages de base
